7750633bccf71797fe9aa72634b2d274639dac79
mysoyul <vega2k_@naver.com>
2025-02-18 11:18:13 +0900

    mapper add

--- src/test/java/myspring/user/JdbcTest.java
+++ src/test/java/myspring/user/JdbcTest.java
@@ -5,17 +5,46 @@ import java.sql.DatabaseMetaData;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import myspring.user.dao.mapper.UserMapper;
+import myspring.user.vo.UserVO;
+
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring_beans.xml")
 public class JdbcTest {
 	@Autowired
 	DataSource dataSource;
+	
+	@Autowired
+	SqlSessionFactory sessionFactory;
+	
+	@Autowired
+	SqlSession session;
+	
+	@Autowired
+	UserMapper userMapper;
+	
+	@Test
+	void userMapper() {
+		UserVO user = userMapper.selectUserById("dooly");
+		System.out.println(user);
+	}
+	
+	
+	@Test @Disabled
+	void mybatis() {
+		System.out.println(sessionFactory.getClass().getName());
+		UserVO user = session.selectOne("userNS.selectUserById", "dooly");
+		System.out.println(user);		
+	}
 	
 	@Test
 	void conn() throws Exception{
